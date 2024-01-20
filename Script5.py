73949413005a303a9b1e66308f8d1fcd3d9d4e65
Zconj095 <96359501+Zconj095@users.noreply.github.com>
2024-01-20 15:45:46 -0500

    Create Script5.py

---
+++ Script5.py
@@ -0,0 +1,703 @@
+# Set up Python environment
+import numpy as np  
+import scipy as sp
+import matplotlib.pyplot as plt
+
+# Bioenergetics module
+class Mitochondria:
+    def __init__(self, membrane_potential=-150): 
+        self.membrane_potential = membrane_potential  
+        
+    def produce_ATP(self):
+        atp = 100 * np.exp(self.membrane_potential/1e3)
+        return atp
+    
+# Synapse biology class    
+class Synapse:
+    
+    def __init__(self, num_receptors=100):
+        self.num_receptors = num_receptors
+        
+    def ltp(self):
+        self.num_receptors += 1
+        
+    def ltd(self):
+        self.num_receptors -= 1 
+
+# Neural population dynamics
+class Neuron:
+    
+    def __init__(self, voltage=-70): 
+        self.voltage = voltage
+        
+    def update(self, current):
+        self.voltage += 0.5*current
+        
+
+excitatory = Neuron()  
+inhibitory = Neuron()
+
+# Manifold learning 
+from sklearn.manifold import TSNE 
+
+class ExperienceMapper:
+    
+    def __init__(self, num_dimensions=3):
+        self.num_dimensions = num_dimensions
+        self.model = TSNE(n_components=num_dimensions)
+        
+    def embed_data(self, data):
+        return self.model.fit_transform(data)
+    
+# Recurrent encoder model
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+class ContinuityEncoder:
+    
+    def __init__(self, sequence_len=5):
+        self.model = Sequential()
+        self.model.add(LSTM(32, input_shape=(sequence_len, 1)))
+        self.model.add(Dense(sequence_len)) 
+    
+    def train(self, sequences):
+        self.model.fit(sequences, sequences, epochs=10)
+        
+    def predict(self, seq_start):
+        return self.model.predict(seq_start)
+    
+# Information flow   
+import networkx as nx 
+
+class InformationFlow:
+    
+    def __init__(self, layers):
+        self.graph = nx.DiGraph()
+        self.add_layers(layers)
+        
+    def add_layers(self, layers):
+        self.graph.add_node
+        
+# Oscillation analyzer
+from scipy import signal
+
+class OscillationAnalyzer:
+
+    def __init__(self, ts, values):
+        self.ts = ts
+        self.values = values
+
+    def lomb_scargle(self, freqs):
+        return signal.lombscargle(self.ts, self.values, freqs) 
+
+# Microbiome signaling 
+import pandas as pd
+
+class Microbiome:
+    
+    def __init__(self, taxa_abundances):
+        self.dataframe = pd.DataFrame(taxa_abundances) 
+        
+    @property
+    def diversity(self):
+        return self.dataframe.shape[1]
+
+    def signaling_cascade(self, metabolites):
+        return [met * (1 + 0.1*self.diversity) for met in metabolites]
+
+# Optogenetics experiment    
+from scipy import optimize
+
+class OptogeneticsModel:
+
+    def __init__(self, opsins, wavelengths):
+       self.opsins = opsins
+       self.wavelengths = wavelengths
+
+    def stimulate(self, intensities):
+
+        def sigmoid(x):  
+            return 1 / (1 + np.exp(-x))
+    
+        def neural_activity(params, wavelengths, intensities):
+            rates = [params[i] * sigmoid(intensities[i]) for i in range(len(self.opsins))]    
+            activity = sum(rates)
+            return activity
+
+        params = np.random.rand(len(self.opsins))    
+        res = optimize.minimize(lambda p: neural_activity(p, self.wavelengths, intensities), params)
+        
+        return res.x # Return optogenetic model parameters
+    
+# Quantum stochastic walk
+import qutip
+
+class QuantumWalk:
+
+    def __init__(self, nodes): 
+        self.num_nodes = len(nodes)
+        self.graph = qutip.graph.graph(nodes)
+
+    def evolve(self, time, system):
+        output = qutip.mesolve(system, time, [], [self.graph], progress_bar=None)
+        return output.states  
+
+# Microtubule dynamics
+from scipy.integrate import odeint
+
+class Microtubule:
+
+    def __init__(self, length=100):
+        self.length = length
+        self.positions = [i for i in range(length)]
+
+    def simulate(self, time):
+        def deriv(state, t):
+            rates = [0.5]*len(state)
+            return rates  
+
+        states = odeint(deriv, self.positions, time)
+        self.positions = states[-1,:]
+        self.length = len(self.positions)
+
+# Experience optimization
+import torch
+
+class ExperienceOptimizer:
+
+    def __init__(self, params):
+        self.params = torch.nn.Parameter(torch.randn(*params) / 2)
+        
+    def update(self, grads): 
+        opt = torch.optim.Adam([self.params], lr=0.05)
+        opt.zero_grad()
+        self.params.sum().backward()  
+        opt.step()   
+
+    @property
+    def loss(self):
+        return -self.params.sum()**2
+    
+# Astrocyte dynamics
+import networkx as nx
+import numpy as np
+
+class AstrocyteNetwork:
+    
+    def __init__(self, num_astrocytes=100, p_connect=0.1):
+        self.g = nx.erdos_renyi_graph(num_astrocytes, p_connect)
+
+    def simulate(self, timesteps):
+        states = [np.random.rand(len(self.g)) for t in range(timesteps)]  
+        return np.array(states) 
+
+# Stochastic resonance
+from scipy import signal
+
+def stochastic_resonance(signal, noise):
+    snr = signal / noise
+    return snr * noise
+
+# Metaphor parser
+import spacy
+
+nlp = spacy.load('en_core_web_lg')
+
+class MetaphorParser:
+    
+    def __init__(self, metaphor):
+        self.doc = nlp(metaphor)
+        
+    def extract(self): 
+        return [(e.text, e.label_) for e in self.doc.ents]
+    
+# Ensemble dynamics
+from scipy.integrate import odeint
+
+def ensemble(state, t, coupling=0.1):  
+    x, y = state  
+    derivs = [-x + coupling*y, 
+              -y + coupling*x]
+    return derivs
+
+# Cerebral blood flow dynamics
+import math
+from scipy.integrate import odeint
+
+class BloodFlowModel:
+
+    def flow_rate(self, p_in, p_out, r, l): 
+        return (p_in - p_out) / (r * l)
+
+    def pressure_drop(self, flow, r, l):  
+        return flow * r * l
+
+    def simulate(self, timesteps):
+        
+        def deriv(p, t):
+            r = 0.1 # vascular resistance 
+            l = 0.5 # vessel length
+            q_in = 5 # mL/s
+            
+            dq1_dt = self.flow_rate(p[0], p[1], r, l)
+            dp1_dt = self.pressure_drop(dq1_dt, r, l)
+            
+            return [dp1_dt, -dp1_dt] 
+
+        state0 = [100, 0]  
+        t = np.linspace(0, 10, timesteps)      
+        p = odeint(deriv, state0, t)  
+
+        return p[:,0] # Return pressure in vessel 1
+        
+# Neurogenesis dynamics
+import numpy as np  
+
+def neurogenesis(progenitors, differentiation_rate, apoptosis_rate):
+
+    n_neurons = 0
+    n_progenitors = progenitors
+    
+    for day in range(365):  
+        n_differentiated = differentiation_rate * n_progenitors
+        n_apoptosis = apoptosis_rate * n_neurons
+        
+        n_progenitors += -n_differentiated 
+        n_neurons += n_differentiated - n_apoptosis
+
+    return n_neurons   
+
+# Spin dynamics
+import qutip 
+
+def spin_simulation(timevector, hamiltonian):
+  
+    initial_state = qutip.basis(2,0)
+    result = qutip.sesolve(hamiltonian, initial_state, timevector)   
+    return result
+
+# Impulse control network 
+import networkx as nx
+
+class ImpulseControlNetwork:
+    
+    def __init__(self, num_nodes=100):
+        self.g = nx.scale_free_graph(num_nodes)
+        
+    def activate(self):
+        self.g.remove_edges_from(self.g.edges())
+        nx.k_edge_augmentation(self.g, 6)
+        return nx.dag_longest_path_length(self.g)
+
+# Transcranial stimulation        
+import numpy as np
+
+class TCS:
+    
+    def __init__(self, power=1):
+        self.waveform = {
+            'tDCS': lambda t: power if (t > 10 and t < 20) else 0,
+            'tACS': lambda t: np.sin(2*np.pi*t)
+        }
+        
+    def stimulate(self, intervals):
+        waveform = self.waveform['tACS'] # Assume tACS chosen
+        return [waveform(t) for t in intervals]
+        
+# Neurotransmitters          
+from scipy.integrate import odeint
+
+class NeurotransmitterModel:
+
+    def derive(self, GLU, DA, SE, NETs):
+        dg_dt = (-2)*GLU + SE
+        dd_dt = (-6)*DA**2 + 3  
+        dse_dt = (-.5)*SE + (GLU/2)  
+        return dg_dt, dd_dt, dse_dt
+
+    def simulate(self, init_conditions, timesteps):
+        output = odeint(self.derive, init_conditions, timesteps)
+        return output[:, 0] # return GLU
+    
+# Synaptic sampling
+import numpy as np
+
+class SynapseFilter:
+
+    def __init__(self, weights):
+        self.weights = np.abs(weights)
+        self.threshold = 0.1
+        
+    def sample(self, n_samples):
+        probabilities = self.weights / sum(self.weights) 
+        idxs = np.random.choice(len(self.weights), n_samples, p=probabilities)  
+        return np.take(self.weights, idxs)
+        
+# Neurogenesis factors       
+class NeurogenesisRegulator:
+
+    def __init__(self, factors):
+        self.factors = factors  # Assume iterable       
+        self.levels = dict(zip(self.factors, [0]*len(factors)))
+        
+    def update(self, factor, amount):
+        self.levels[factor] += amount
+        
+    def propagate(self):
+        for f in self.factors:
+            if self.levels[f] > 0.1:
+                self.__trigger_neurogenesis()
+                break
+                
+    def __trigger_neurogenesis(self):
+        print("Neurogenesis activated!")
+        
+# Structural connectivity
+import numpy as np
+        
+class StructuralNetwork:
+
+    def __init__(self, regions):
+        self.num_regions = len(regions)
+        self.sc = np.random.randn(self.num_regions, self.num_regions) 
+        
+    def reinforce_path(self, path, weight_increase=0.5):
+        weights = []
+        for i in range(1, len(path)):
+            w = self.sc[path[i-1], path[i]] 
+            self.sc[path[i-1], path[i]] += weight_increase
+            weights.append(w)
+        return weights
+    
+
+# Brainwave sampling
+from scipy import signal
+import numpy as np
+
+
+class BrainWaves:
+
+    def __init__(self, freqs):
+        self.ts = np.linspace(0, 10, 1000)
+        self.freqs = freqs  
+        self.waves = {
+            'delta': self.make_wave(2, 1.5),
+            'theta': self.make_wave(5, 2),
+            'alpha': self.make_wave(11, 2.5),
+        }
+
+    def make_wave(self, freq, amplitude):
+        return amplitude * np.sin(freq * self.ts)
+
+    def measure(self, regions):
+        return {r: self.sample()[r] for r in regions}
+
+    def sample(self): 
+        sample_freqs = np.random.choice(self.freqs, 3)
+        return dict(zip(self.waves.keys(),  
+                        [self.waves[k][np.random.randint(1000)] for k in sample_freqs]))
+                        
+        
+# Neurogenesis timecourse         
+def simulate_neurogenesis(dt=0.1, duration=30):
+    
+    G = 10
+    Z = 100
+    D = 50
+    
+    def dgdt(G, Z, D):
+        a = 0.1
+        return a*Z - a*G
+      
+    def dzdt(Z, D):    
+        k = 0.05
+        return -k*D*Z
+    
+    def dddt(D, Z):
+        r = 0.025
+        b = 0.2 
+        return r*D + b*Z - 1.5*D
+      
+    state = [G, Z, D]
+    times = np.arange(0, duration, dt)  
+    states = np.zeros((len(times), 3))  
+    states[0] = state
+    
+    for i,t in enumerate(times[1:]):
+        state = state + np.array([f(state[0],state[1],state[2])*dt 
+                                  for f in [dgdt, dzdt, dddt]])     
+        states[i+1] = state
+        
+    return times, states  
+
+  
+# Neural field dynamics
+from scipy.integrate import odeint  
+
+def spread(state, t):
+        
+    V,W = state
+    tau = 10
+    gamma = 0.5
+    Iext = 1 
+    
+    dVdt = (V - V**3/3 - W + Iext)/tau
+    dWdt = (gamma*(V - beta*W))/tau
+  
+    return [dVdt, dWdt]
+
+
+# Ion channel noise
+import numpy as np
+from scipy import signal
+
+class NoisyChannel:
+    
+    def __init__(self, n=1):
+        self.amplitude = 0.7
+        self.n = n # Number of channels
+        self.noise = []
+        
+    def generate(self, t, fc=100, dc=0.3):
+        for i in range(self.n):
+            fft_num = self.amplitude * np.abs(signal.gausspulse(t, fc=fc, retquad=False, retenv=False))
+            self.noise.append(fft_num + dc*np.random.randn(*t.shape)) 
+        return self.noise 
+    
+    def power(self):        
+        ps = np.abs(np.fft.rfft(self.noise)) ** 2
+        return ps 
+
+# Neurofeedback    
+from brainflow.board_shim import BoardShim, BrainFlowInputParams     
+from brainflow.data_filter import DataFilter, FilterTypes
+
+class Neurofeedback:
+
+    def __init__(self, device='muse', channels=[0,1,2]):        
+        self.board = BoardShim(device, BrainFlowInputParams())        
+        self.board.prepare_session()        
+        self.filter = DataFilter(FilterTypes.BUTTERWORTH.value, channels)   
+        self.channels = channels
+
+    def get_data(self):
+        data = self.board.get_board_data() 
+        filtered = self.filter.filter(data)
+        return filtered[self.channels,:]       
+
+
+# Morphogens
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+class MorphogenReactionDiffusion:
+    
+    def __init__(self, size):
+        self.size = size
+
+    def fitzhugh_nagumo(self, state, t):
+
+        u, v = state
+        Du, Dv = 1, 0
+
+        dx = 1
+        dt = 0.03
+        
+        f = u - u**3 - v
+        g = u + a
+        
+        dudt = Du*d2udx2 + f         
+        dvdt = Dv*d2vdx2 + g 
+
+        return dudt, dvdt
+
+    def simulate(self, timesteps):
+    
+        init_cond = [-1]*(self.size//2) + [1]*(self.size//2), [0.1]*self.size
+        grid = np.linspace(0, 1, self.size)  
+
+        output = np.empty((self.size, timesteps))
+        output[:,0] = init_cond
+
+        for i in range(timesteps-1):
+            derivs = self.fitzhugh_nagumo(output[:,i], i)
+            output[:,i+1] = euler_step(output[:,i], derivs, grid, 0.001)
+
+        return output
+    
+
+# Neural embedding        
+import torch
+from torch import nn
+
+class NeuralEmbedding(nn.Module):
+
+    def __init__(self, in_dim, out_dim):
+        self.linear = nn.Sequential(
+            nn.Linear(in_dim, 64),
+            nn.ReLU(),  
+            nn.Linear(64, out_dim) 
+        )
+
+    def forward(self, x):
+        return self.linear(x) 
+    
+        
+# Hebbian Learning
+import numpy as np    
+
+class HebbianNet:
+
+    def __init__(self, num_inputs):
+        self.weights = np.random.rand(num_inputs)  
+
+    def train(self, inputs):
+        return np.dot(self.weights, inputs)
+
+    def update(self, inputs):
+        self.weights += inputs # Hebbian learning
+        
+# Glial dynamics
+import numpy as np
+
+class GlialCell:
+
+    def __init__(self):
+        self.Ca = 0.1  
+
+    def uptake(self, glutamate):
+        self.Ca += 0.1*glutamate  
+
+    def gliotransmission(self):
+        if self.Ca > 0.2:
+            self.Ca -= 0.1
+            return True
+        return False
+        
+
+class AstrocyteNetwork:
+    
+    def __init__(self, n=20):  
+        self.astrocytes = [GlialCell() for _ in range(n)]
+
+    def simulate(self, glutamate, timesteps):
+        
+        gliotransmissions = []
+        
+        for t in range(timesteps):
+            glut = np.random.choice(glutamate)  
+            astro = np.random.choice(self.astrocytes)  
+            astro.uptake(glut)
+            
+            if astro.gliotransmission():
+                gliotransmissions.append(astro)  
+                
+        return gliotransmissions[-5:] if gliotransmissions else []
+
+    
+# Spin ensemble        
+import qutip
+
+def ensemble_dynamics(t, num_spins=5, couplings=[2,-1,3,-0.5]):
+    
+    dims = [2 for i in range(num_spins)]
+    states = qutip.tensor([qutip.basis(2) for i in dims])
+    
+    H = qutip.tensor([qutip.sigmaz() for i in range(num_spins)])
+
+    for i in range(num_spins):
+        op1 = qutip.tensor(qutip.identity(2), [(i,qutip.sigmaz()),
+                                               (i+1,qutip.sigmaz())])  
+        H += couplings[i] * op1
+        
+    return qutip.mesolve(H, states, t).expect[0]
+
+
+import numpy as np
+from scipy import signal
+
+# Auric emission signal generation 
+class AuraSource:
+    
+    def __init__(self):
+        self.signal = {
+            'freq': np.random.randint(1, 12),
+            'phase': np.random.rand(),
+            'amplitude': np.random.rand() 
+        }
+        self.raw = self.generate_signal()
+        
+    def generate_signal(self):
+        n = np.random.randn(100) 
+        freq, phase, amplitude = self.signal.values() 
+        x = amplitude * np.sin((freq/1.3)*phase) + n
+        return x  
+        
+class SensorArray:
+    
+    def __init__(self):
+        self.eeg = []
+        self.ekg = []
+        
+    def measure(self):
+        self.eeg.append(np.random.rand(20))
+        self.ekg.append(np.random.randn(5) + 10)
+        
+    @property
+    def bpm(self):
+        peaks,_ = signal.find_peaks(self.ekg[-1], distance=8)  
+        bpm = (len(peaks)-1)/4
+        return bpm
+    
+# Layered auric topology 
+from scipy.spatial import ConvexHull
+points = [[1,3],[3,4],[5,7],[3,1],[6,7],[5,2]]
+
+def calculate_layers(points):    
+    hull = ConvexHull(points)
+    return hull.area
+
+# Energetic resonance modeling
+from scipy import signal  
+import matplotlib.pyplot as plt
+
+def resonant_interactions(t, phase=(0,np.pi/2)): 
+    
+    ft1 = 1  
+    ft2 = 3
+    
+    f1 = 3*np.sin(ft1*(t+phase[0]))  
+    f2 = np.sin(ft2*(t+phase[1]))
+    
+    interaction =  f1*f2 
+    
+    return f1, f2, interaction
+    
+t = np.linspace(0, 20, 1000)
+f1, f2, interaction = resonant_interactions(t)  
+
+plt.plot(t, f1, label='Source')
+plt.plot(t, f2 , label='Recipient') 
+plt.plot(t, interaction, label = 'Interaction')
+plt.legend()
+plt.xlabel('Time (s)')
+plt.show()
+
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+t = np.linspace(0, 10, 100)
+line, = ax.plot(t, np.sin(t))
+
+def animate(i):
+    line.set_ydata(np.sin(2 * np.pi * (t + 0.01 * i)))  
+    return line,
+
+def init():  
+    line.set_ydata(np.sin(t))
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, init_func=init, frames=200, 
+                              interval=20, blit=True)
+plt.show()
